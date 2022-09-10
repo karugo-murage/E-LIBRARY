a496376e79c335c4768431883b9e7de473b48b0f
karugo-murage <murage.gidion@moringaschool.com>
2022-09-10 20:10:41 +0300

    Add navbar

---
+++ exam_library/src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React  from 'react'
+import { NavLink } from 'react-router-dom'
+
+ 
+ export default function Navbar() {
+   return (
+     <div>
+        <div className='navbar'>
+            <h1 className='success'>SUCCESS IN YOUR EXAMS .</h1>
+            <div className='navlink-bar'>
+            <NavLink className="routes_nav" to ="home">
+              Home
+            </NavLink>
+            <NavLink className="routes_nav"to ="about">
+              About
+            </NavLink>
+            <NavLink className="routes_nav" to ="contact">
+              Contact
+            </NavLink>
+            </div>
+    
+        </div>
+        
+     </div>
+   )
+ }
