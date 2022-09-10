e94355d7f949f78cfc6f5786e6591edf5d34c513
karugo-murage <murage.gidion@moringaschool.com>
2022-09-10 20:09:07 +0300

    Add component Experience

---
+++ exam_library/src/components/Experience.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Experience() {
+  return (
+    <div className='post' >
+        <h3 className='post-tag' >Fell free to share your experience after you sat for the exam.</h3>
+        
+    </div>
+  )
+}
